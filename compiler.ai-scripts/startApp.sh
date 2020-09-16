2cec3c399df53e0eedc95ab71c6f5100421ef58a
Sorav Bansal <sbansal@iitd.ac.in>
2020-09-16 19:59:45 +0530

    compiler.ai-scripts/startApp:enable output logging

--- compiler.ai-scripts/startApp.sh
+++ compiler.ai-scripts/startApp.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 ROOT=/superopt-project
-cd $ROOT && screen -d -m make -C compiler.ai-scripts/compiler-explorer && cd -
+timestamp=$(date +'%Y.%m.%d.%H.%M')
+cd $ROOT && screen -L -Logfile compilerai.log.${timestamp} -d -m make -C compiler.ai-scripts/compiler-explorer && cd -
