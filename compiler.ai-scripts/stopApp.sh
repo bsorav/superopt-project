b82696dc917920e3b991277f46662a97a13db815
Sorav Bansal <sbansal@iitd.ac.in>
2020-12-31 18:11:20 +0530

    Fixes to compilerai scripts

--- compiler.ai-scripts/stopApp.sh
+++ compiler.ai-scripts/stopApp.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-sudo -u compilerai-server {ps aux | grep -i node | awk '{print $2}' | xargs  kill -SIGINT}
+
+set -o xtrace
+
+sudo -H -E -u compilerai-server bash -c "ps aux | grep -i node | awk '{print \$2}' | xargs  kill -SIGINT"
 exit 0
