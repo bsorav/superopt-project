cf34655613be7b401af71a7cacd2c5a9e46520d1
Sorav Bansal <sbansal@iitd.ac.in>
2020-12-31 15:39:02 +0530

    fix to startApp.sh

--- compiler.ai-scripts/startApp.sh
+++ compiler.ai-scripts/startApp.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
+set -o xtrace
+
 PARENTDIR=/home/compilerai-server
 ROOT=$PARENTDIR/superopt-project
+
 timestamp=$(date +'%Y.%m.%d.%H.%M')
-sudo -u compilerai-server {cd $ROOT && screen -L -Logfile compilerai.log.${timestamp} -d -m make -C compiler.ai-scripts/compiler-explorer && cd -}
+cd $ROOT && sudo -u compilerai-server screen -L -Logfile compilerai.log.${timestamp} -d -m make -C compiler.ai-scripts/compiler-explorer && cd -
