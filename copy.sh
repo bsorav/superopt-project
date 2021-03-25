07a63d5dd62ba5e091e785e990af4c9ebb735a04
Aditya Senthilnathan <adityanathan@jupiter.cse.iitd.ernet.in>
2021-03-25 09:26:35 +0530

    Fixed bug where eq32 was interpreting .ll files as .c

---
+++ copy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp $HOME/superopt-project/superopt-tests/build/paper_ex/paper_ex_src.bc32.eqchecker.O0.ll $HOME/superopt-project/mytests/paper_ex_src.bc32.eqchecker.O0.ll
+cp $HOME/superopt-project/superopt-tests/build/paper_ex/paper_ex_dst.gcc.eqchecker.O0.i386.s $HOME/superopt-project/mytests/paper_ex_dst.gcc.eqchecker.O0.i386.s
