d263fba09504baf82e8e7fcae30fef685e5c741b
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2023-03-14 01:58:21 +0530

    lab1_test/run.sh: debug edit

--- lab1_test/run.sh
+++ lab1_test/run.sh
@@ -12,5 +12,5 @@ fname=$1
 fname_basename=$(basename "${fname}")
 fname_etfg=${fname_basename}.etfg
 
-#${ROOT}/llvm-project/build/bin/llvm2tfg "${fname}" -o "${fname_etfg}" --dyn_debug=anticipated_analysis
+#${ROOT}/llvm-project/build/bin/llvm2tfg "${fname}" -o "${fname_etfg}" --dyn_debug=anticipated_analysis,lazy_code_motion
 ${ROOT}/llvm-project/build/bin/llvm2tfg "${fname}" -o "${fname_etfg}" | tee last_run.log
