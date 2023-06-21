b397b08645f6d739782bb8e48e996a5360a47ce3
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2023-06-22 00:28:40 +0530

    install-dependencies.sh: rm unneeded packages

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -10,22 +10,22 @@ then
   exit
 fi
 
-build="build-essential cmake flex bison unzip ninja-build python3-pip git lld"
-llvm="llvm llvm-dev clang-11"
+build="make cmake flex bison unzip ninja-build python3 python3-pip git"
+llvm="llvm-11 llvm-11-dev clang-11 lld-11"
 libs="gcc-multilib g++-multilib libiberty-dev binutils-dev zlib1g-dev libgmp-dev libelf-dev libmagic-dev libssl-dev libswitch-perl ocaml-nox lib32stdc++-8-dev"
 yices="gperf libgmp3-dev autoconf"
-superopt="expect libtirpc-dev libtirpc3 libtirpc-common rpcbind z3 libz3-dev libyaml-cpp0.6 libyaml-cpp-dev"
-db="ruby ruby-dev gem freetds-dev"
-tests="libc6-dev-i386 gcc-8-multilib g++-8-multilib linux-libc-dev:i386 parallel python3"
+superopt="expect libtirpc-dev libtirpc3 libtirpc-common rpcbind libyaml-cpp0.6 libyaml-cpp-dev"
+tests="g++-8 libc6-dev-i386 gcc-8-multilib g++-8-multilib linux-libc-dev:i386 parallel"
 
-apt-get install -y $build $llvm $libs $yices $superopt $db $tests
+apt-get install -y $build $llvm $libs $yices $superopt $tests
 
 # optional
+db="ruby ruby-dev gem freetds-dev"
 fbgen="camlidl"
 compcert="menhir ocaml-libs"
-compiler_explorer="python3-distutils gcc g++ make"
+compiler_explorer="python3-distutils gcc g++"
 suggested="cscope exuberant-ctags atool emacs"
-# apt-get install -y $fbgen $compcert $compiler_explorer $suggested
+# apt-get install -y $fbgen $compcert $compiler_explorer $suggested $db 
 
 #following is for db
 command -v gem && gem install tiny_tds || true
