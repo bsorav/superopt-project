fe6b8023ced68ac1ef2fb19e1d1c5bde2852c12f
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2023-08-04 18:21:26 +0530

    install-dependencies.sh: bumped compiler version

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -11,7 +11,7 @@ then
 fi
 
 build="make cmake flex bison unzip ninja-build python3 python3-pip git"
-llvm="llvm-11 llvm-11-dev clang-11 lld-11"
+llvm="llvm-12 llvm-12-dev clang-12 lld-12"
 libs="gcc-multilib g++-multilib libiberty-dev binutils-dev zlib1g-dev libgmp-dev libelf-dev libmagic-dev libssl-dev libswitch-perl ocaml-nox lib32stdc++-8-dev"
 yices="gperf libgmp3-dev autoconf"
 superopt="expect libtirpc-dev libtirpc3 libtirpc-common rpcbind libyaml-cpp0.6 libyaml-cpp-dev"
