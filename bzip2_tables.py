e23ddd31bd3ff9f40c2d33849fb17f7168156959
Abhishek Rose <abhishek.rose@cse.iitd.ac.in>
2024-01-01 13:14:28 +0530

    bzip2_tables.py; minor fix

--- bzip2_tables.py
+++ bzip2_tables.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     target_functions = { 'recvDecodingTables', 'generateMTFValues', 'undoReversibleTransformation_fast' }
-    short_tab =  gen_detailed_table(args.d + '/bzip2_O1-.csv', target_functions)
+    short_tab =  gen_detailed_table(args.dir + '/bzip2_O1-.csv', target_functions)
     print(short_tab)
 
-    long_tab = gen_passing_latex_table([args.d + '/bzip2_O1-.csv', args.d + '/bzip2_O1.csv', args.d + '/bzip2_O2.csv'])
+    long_tab = gen_passing_latex_table([args.dir + '/bzip2_O1-.csv', args.dir + '/bzip2_O1.csv', args.dir + '/bzip2_O2.csv'])
     print(long_tab)
