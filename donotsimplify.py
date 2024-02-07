bb823a07b9ca43e1762e1d5ccff6bd951bc24d71
Ramanuj Goel <ramanuj@gmail.com>
2024-02-07 21:32:48 +0530

    added new donotsimplify things

--- donotsimplify.py
+++ donotsimplify.py
@@ -1,13 +1,77 @@
 
+# vals = [("subsign", 3),
+#         ("suboverflow", 3),
+#         ("subzero", 3)]
+
 vals = [("flagunchanged", 1),
         ("setflags", 8),
-        ("getcarry", 1),
-        ("getadjust", 1),
-        ("getdirection", 1),
-        ("getoverflow", 1),
-        ("getsign", 1),
-        ("getzero", 1),
-        ("getparity", 1),
+        ("getcarry", 2),
+        ("getadjust", 2),
+        ("getdirection", 2),
+        ("getoverflow", 2),
+        ("getsign", 2),
+        ("getzero", 2),
+        ("getparity", 2),
+        
+        ("seteq", 2),
+        ("setne", 2),
+        ("setult", 2),
+        ("setslt", 3),
+        ("setugt", 3),
+        ("setsgt", 4),
+        ("setule", 3),
+        ("setsle", 4),
+        ("setuge", 2),
+        ("setsge", 3),
+        
+        # ("geteq", 2),
+        # ("getne", 2),
+        # ("getult", 2),
+        # ("getslt", 2),
+        # ("getugt", 2),
+        # ("getsgt", 2),
+        # ("getule", 2),
+        # ("getsle", 2),
+        # ("getuge", 2),
+        # ("getsge", 2),
+        
+        ("logicsign", 2),
+        ("logiczero", 2),
+        ("logicparity", 2),
+
+        ("bextres", 3),
+        ("bextzero", 3),
+        
+        ("blsires", 2),
+        ("blsicf", 2),
+        ("blsisign", 2),
+        ("blsizero", 2),
+        
+        ("blsmskres", 2),
+        ("blsmskcf", 2),
+        ("blsmsksign", 2),
+        
+        ("blsrres", 2),
+        ("blsrcf", 2),
+        ("blsrsign", 2),
+        ("blsrzero", 2),
+
+        ("bzhires", 3),
+        ("bzhicf", 3),
+        ("bzhisign", 3),
+        ("bzhizero", 3),
+        
+        ("floatcmpparity", 3),
+        ("floatcmpzero", 3),
+        ("floatcmpcarry", 3),
+        ("floatcmpne", 3),
+        ("floatcmpugt", 3),
+        ("floatcmpule", 3),
+        ("floatcmpuge", 3),
+        
+        ("readmem", 2),
+        # ("writemem", 3),
+        
         ("subdiffreg", 3),
         ("subcarry", 3),
         ("subadjust", 3),
@@ -15,6 +79,7 @@ vals = [("flagunchanged", 1),
         ("subsign", 3),
         ("subzero", 3),
         ("subparity", 3),
+        
         ("addsumreg", 3),
         ("addcarry", 3),
         ("addadjust", 3),
@@ -22,6 +87,7 @@ vals = [("flagunchanged", 1),
         ("addsign", 3),
         ("addzero", 3),
         ("addparity", 3),
+        
         ("shiftres", 3),
         ("shiftcfbit", 3),
         ("shiftafbit", 3),
@@ -29,11 +95,15 @@ vals = [("flagunchanged", 1),
         ("shiftsfbit", 3),
         ("shiftzfbit", 3),
         ("shiftpfbit", 3),
+        
         ("rotateres", 3),
         ("rotatecfbit", 3),
         ("rotateofbit", 3),
+        
         ("mulhigh", 3),
         ("mullow", 3),
+        ("mulnotzero", 3),
+        
         ("divquotient", 3),
         ("divremainder", 3),
         ("testzfbit", 3),
@@ -113,10 +183,7 @@ def parse4():
     filename = "./superopt/include/expr/context.h"
     text = open(filename).read()
     findstart = "  expr_ref mk_donotsimplify_using_solver_"
-    findend = """
-  
-  //expr_ref mk_function_argument_ptr(expr_ref a, memlabel_t const &ml, expr_ref mem, expr_id_t suggested_id = 0);
-  expr_ref mk_return_value_ptr(expr_ref a, memlabel_t const &ml, expr_ref mem, expr_id_t suggested_id = 0);"""
+    findend = """//expr_ref mk_function_argument_ptr(expr_ref a, memlabel_t const &ml, expr_ref mem, expr_id_t suggested_id = 0);"""
     idx1 = text.find(findstart)
     idx2 = text.find(findend, idx1)
     new_text = text[:idx1]
@@ -186,8 +253,7 @@ def parse7():
     filename = "./superopt/lib/expr/expr.cpp"
     text = open(filename).read()
     findstart = """    add_entry("donotsimplify_using_solver_"""
-    findend = """    
-    add_entry("memmask", expr::OP_MEMMASK);"""
+    findend = """    add_entry("memmask", expr::OP_MEMMASK);"""
     idx1 = text.find(findstart)
     idx2 = text.find(findend, idx1)
     new_text = text[:idx1]
@@ -208,8 +274,7 @@ def parse8():
     filename = "./superopt/lib/expr/expr.cpp"
     text = open(filename).read()
     findstart = """expr_ref context::mk_donotsimplify_using_solver_"""
-    findend = """
-/*expr_ref context::mk_function_argument_ptr(expr_ref a, memlabel_t const &ml, expr_ref mem, expr_id_t suggested_id)
+    findend = """/*expr_ref context::mk_function_argument_ptr(expr_ref a, memlabel_t const &ml, expr_ref mem, expr_id_t suggested_id)
 {
   return mk_app(expr::OP_FUNCTION_ARGUMENT_PTR, make_args(a, this->mk_memlabel_const(ml), mem));
 }
@@ -241,10 +306,7 @@ def parse9():
     filename = "./superopt/lib/expr/expr_to_z3_expr.cpp"
     text = open(filename).read()
     findstart = """  case expr::OP_DONOTSIMPLIFY_USING_SOLVER_"""
-    findend = """  {
-    ASSERT(e->get_args().size() == 3);
-    expr_ref arg = e->get_args().at(0);
-"""
+    findend = """  {"""
     idx1 = text.find(findstart)
     idx2 = text.find(findend, idx1)
     new_text = text[:idx1]
@@ -258,8 +320,7 @@ def parse10():
     filename = "./superopt/lib/expr/eval.cpp"
     text = open(filename).read()
     findstart = """  case expr::OP_DONOTSIMPLIFY_USING_SOLVER_"""
-    findend = """{
-    pair<expr_eval_status_t, expr_ref> cval0 = m_map.at(e->get_args().at(0)->get_id());"""
+    findend = """{"""
     idx1 = text.find(findstart)
     idx2 = text.find(findend, idx1)
     new_text = text[:idx1]
@@ -314,11 +375,7 @@ def parse13():
     fp = """expr::expr_op_can_be_replaced_by_first_arg(expr::operation_kind op)"""
     idx0 = text.find(fp)
     findstart = "(op == OP_DONOTSIMPLIFY_USING_SOLVER_"
-    findend = """;
-}
-
-bool
-expr::expr_op_is_bvcmp(expr::operation_kind op)"""
+    findend = """;"""
     idx1 = text.find(findstart, idx0)
     idx2 = text.find(findend, idx1)
     new_text = text[:idx1]
@@ -328,6 +385,7 @@ expr::expr_op_is_bvcmp(expr::operation_kind op)"""
             new_text += "(op == OP_DONOTSIMPLIFY_USING_SOLVER_" + v.upper() + ")\n"
         else:
             new_text += "    || (op == OP_DONOTSIMPLIFY_USING_SOLVER_" + v.upper() + ")\n"
+        i += 1
     new_text += text[idx2:]
     open("./superopt/lib/expr/expr.cpp", 'w').write(new_text)
     
@@ -343,3 +401,4 @@ parse9()
 parse10()
 parse11()
 parse12()
+parse13()
